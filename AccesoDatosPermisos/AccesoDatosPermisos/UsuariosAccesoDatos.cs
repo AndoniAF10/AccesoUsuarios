7b92ffe7e84e8604a6c85ae729e1e8ac0a3f515c
AndoniAF10 <cesarandoniafanadorjazoandoni@hotmail.com>
2021-09-23 14:44:54 -0500

    ConexionEntidades

---
+++ AccesoDatosPermisos/AccesoDatosPermisos/UsuariosAccesoDatos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace AccesoDatosPermisos
+{
+    public class UsuariosAccesoDatos
+    {
+        ConexionAccesoDatos _conexion;
+        public UsuariosAccesoDatos()
+        {
+
+            try
+            {
+                _conexion = new ConexionAccesoDatos("localhost", "root", "123456789", "permisos", 3306);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fallo la conexion" + ex.Message);
+            }
+        }
+    }
+}
--- AccesoDatosPermisos/ManejadoresPermisos/Class1.cs
+++ AccesoDatosPermisos/ManejadoresPermisos/Class1.cs
@@ -8,5 +8,6 @@ namespace ManejadoresPermisos
 {
     public class Class1
     {
+ 
     }
 }
