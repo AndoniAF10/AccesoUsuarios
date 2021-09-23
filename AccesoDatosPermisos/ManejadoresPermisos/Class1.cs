82976225f871e7bfffdc3993a66585c869450fc7
AndoniAF10 <cesarandoniafanadorjazoandoni@hotmail.com>
2021-09-23 13:08:16 -0500

    proyectoVacio

---
+++ AccesoDatosPermisos/ManejadoresPermisos/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejadoresPermisos
+{
+    public class Class1
+    {
+    }
+}
